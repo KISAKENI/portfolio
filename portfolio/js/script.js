47c5ed63515e884022acb0ebb1547686c51dde74
KISAKENI <124774264+KISAKENI@users.noreply.github.com>
2024-07-11 23:43:29 -0700

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,7 @@
+/*=======================  typimg animation====================*/
+var typed = new Typed(".typing",{
+    strings:[ "","Web Designer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
